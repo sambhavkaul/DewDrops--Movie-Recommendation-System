de3bfb8a7247dea5c732968592baf587c0e56d9f
Sambhav Kaul <sambhav.kaul12@gmail.com>
2019-06-20 12:35:33 +0530

    Changes V4

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import RegistrationForm, LoginForm
 import pandas as pd
+from rake_nltk import Rake
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
 
@@ -8,40 +9,77 @@ app = Flask(__name__, template_folder="templates")
 
 app.config["SECRET_KEY"] = "97787179b52dea978dcf3c0c474a76ac"
 
-md = pd.read_csv("./data/movies_metadata.csv", low_memory=False, dtype={"id": str})
-links_small = pd.read_csv("./data/links_small.csv")
-links_small = links_small[links_small["tmdbId"].notnull()]["tmdbId"].astype("int")
+df = pd.read_csv("./data/tmdb_5000_movies.csv")
 
-md = md.drop([19730, 29503, 35587])
-md["id"] = md["id"].astype("int")
-smd = md[md["id"].isin(links_small)]
+df = df[["Title", "Genre", "Plot", "vote_average"]]
+df = df.astype(str)
+df["Genre"] = df["Genre"].map(lambda x: x.lower().split(","))
+df["Key_words"] = ""
 
-smd["tagline"] = smd["tagline"].fillna("")
-smd["description"] = smd["overview"] + smd["tagline"]
-smd["description"] = smd["description"].fillna("")
+for index, row in df.iterrows():
+    plot = row["Plot"]
 
-tf = TfidfVectorizer(
-    analyzer="word", ngram_range=(1, 2), min_df=0, stop_words="english"
-)
-tfidf_matrix = tf.fit_transform(smd["description"])
+    # instantiating Rake, by default it uses english stopwords from NLTK
+    # and discards all puntuation characters as well
+    r = Rake()
 
-cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
-smd = smd.reset_index()
-titles = smd["title"]
-indices = pd.Series(smd.index, index=smd["title"])
+    # extracting the words by passing the text
+    r.extract_keywords_from_text(plot)
+
+    # getting the dictionary whith key words as keys and their scores as values
+    key_words_dict_scores = r.get_word_degrees()
+
+    # assigning the key words to the new column for the corresponding movie
+    row["Key_words"] = list(key_words_dict_scores.keys())
+
+# dropping the Plot column
+df.drop(columns=["Plot"], inplace=True)
+df.set_index("Title", inplace=True)
 
 
-def get_recommendations(title):
-    idx = indices[title]
-    sim_scores = list(enumerate(cosine_sim[idx]))
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:31]
-    movie_indices = [i[0] for i in sim_scores]
-    temp = titles.iloc[movie_indices]
-    dm = pd.DataFrame(temp).reset_index()
-    dm.columns = ["Rating", "Title"]
-    dm_toList = dm["Title"].tolist()
-    return dm_toList
+df["bag_of_words"] = ""
+columns = df.columns
+for index, row in df.iterrows():
+    words = ""
+    for col in columns:
+        words = words + " ".join(row["Genre"]) + " " + " ".join(row["Key_words"])
+
+        row["bag_of_words"] = words
+df.drop(columns=["Genre"], inplace=True)
+df.drop(columns=["Key_words"], inplace=True)
+
+count = CountVectorizer()
+count_matrix = count.fit_transform(df["bag_of_words"])
+
+# generating the cosine similarity matrix
+cosine_sim = cosine_similarity(count_matrix, count_matrix)
+
+# creating a Series for the movie titles so they are associated to an ordered numerical
+# list I will use in the function to match the indexes
+indices = pd.Series(df.index)
+
+
+#  defining the function that takes in movie title
+# as input and returns the top 10 recommended movies
+def recommendations(title, cosine_sim=cosine_sim):
+
+    # initializing the empty list of recommended movies
+    recommended_movies = []
+
+    # gettin the index of the movie that matches the title
+    idx = indices[indices == title].index[0]
+
+    # creating a Series with the similarity scores in descending order
+    score_series = pd.Series(cosine_sim[idx]).sort_values(ascending=False)
+
+    # getting the indexes of the 10 most similar movies
+    top_10_indexes = list(score_series.iloc[1:11].index)
+
+    # populating the list with the titles of the best 10 matching movies
+    for i in top_10_indexes:
+        recommended_movies.append(list(df.index)[i])
+
+    return recommended_movies
 
 
 @app.route("/")
@@ -74,7 +112,7 @@ def predict():
 
     if request.method == "POST":
         catch = request.form["catch"]
-        my_prediction = get_recommendations(catch)
+        my_prediction = recommendations(catch)
 
     return render_template("result.html", prediction=my_prediction, first=catch)
 
